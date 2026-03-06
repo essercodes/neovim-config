1d57e351e0d41513f5a3f3ab9a3910876f912b89
Max Esser <max@esser.codes>
2026-03-05 23:05:13 -0600

    trying something new

--- init.lua
+++ init.lua
@@ -1,20 +1,25 @@
 require("config.lazy")
-vim.o.background = 'dark'
+vim.o.background = "dark"
 vim.cmd([[
-    colorscheme gruvbox
+  colorscheme gruvbox
 	set relativenumber number
 	syntax on
 	set ruler
 	retab
-	set softtabstop=4
+	set softtabstop=2
 	set cc=80
 	set expandtab
-	set tabstop=4
-	set shiftwidth=4
+	set tabstop=2
+	set shiftwidth=2
 
 	augroup make_settings " {
 	    autocmd!
 	    autocmd BufNewFile,BufRead Makefile set noexpandtab
 	augroup END " }
+
 ]])
 
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
