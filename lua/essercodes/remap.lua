3bffcdfbdefa99ced492bb5156a00aa0890daaba
Max Esser <max@esser.codes>
2026-03-10 15:04:07 -0500

    added an explorer

--- lua/essercodes/remap.lua
+++ lua/essercodes/remap.lua
@@ -1,4 +1,11 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", function()
+  local file = vim.fn.expand('%:p')
+  if vim.fn.filereadable(file) == 1 then
+    vim.cmd("Neotree action=focus position=float reveal_file=" .. file)
+  else
+    vim.cmd("Neotree action=focus position=float")
+  end
+end)
 vim.g.maplocalleader = "\\"
-  
+
