ed253bd9598be64a64fb17ac044f804452578fa6
Max Esser <max@esser.codes>
2026-03-10 15:49:55 -0500

    added remaps and set options

--- lua/essercodes/remap.lua
+++ lua/essercodes/remap.lua
@@ -1,4 +1,4 @@
-vim.g.mapleader = " "
+-- Open file explorer
 vim.keymap.set("n", "<leader>pv", function()
   local file = vim.fn.expand('%:p')
   if vim.fn.filereadable(file) == 1 then
@@ -7,5 +7,23 @@ vim.keymap.set("n", "<leader>pv", function()
     vim.cmd("Neotree action=focus position=float")
   end
 end)
-vim.g.maplocalleader = "\\"
+
+-- Move highlighted text up/down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Keep cursor in place when appending next line
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- Center cursor after moving
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzze")
+vim.keymap.set("n", "n", "nzzze")
+vim.keymap.set("n", "n", "nzzze")
+
+vim.keymap.set("n", "<leader>c", function()
+    vim.lsp.buf.rename()
+end, {desc = "LSP Rename"})
 
--- lua/essercodes/set.lua
+++ lua/essercodes/set.lua
@@ -1,7 +1,29 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
 vim.o.number=true
 vim.o.relativenumber=true
 vim.o.ruler=true
-vim.o.colorcolumn="80,100"
+vim.o.colorcolumn="80"
+
+vim.o.smartindent=true
+vim.o.wrap=false
+
+vim.o.swapfile=false
+vim.o.backup=false
+vim.o.undodir=os.getenv("HOME") .. "/.vim/undotree"
+vim.o.undofile=true
+
+vim.o.hlsearch=false
+vim.o.incsearch=true
+
+vim.o.termguicolors=true
+
+vim.o.scrolloff=8
+vim.o.signcolumn='yes'
+
+vim.o.updatetime=50
+
 vim.o.expandtab=true
 vim.o.shiftwidth=4
 vim.o.tabstop=4
@@ -10,3 +32,4 @@ vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
     pattern = 'Makefile',
     command = 'set noexpandtab',
 })
+
