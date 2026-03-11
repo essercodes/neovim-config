60f77f17ee469f0ae6b1f806fa9203910f15e68f
Max Esser <max@esser.codes>
2026-03-11 16:20:36 -0500

    added keybind to fzf filenames and copy to default register

--- lua/essercodes/lazy/telescope.lua
+++ lua/essercodes/lazy/telescope.lua
@@ -5,15 +5,24 @@ return {
 		"nvim-lua/plenary.nvim",
 		-- optional but recommended
 		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		{ "nvim-telescope/telescope-ui-select.nvim" },
 	},
 
 	config = function()
-		require("telescope").setup({})
+		require("telescope").setup({
+			extensions = {
+				["ui-select"] = {
+					require("telescope.themes").get_dropdown({}),
+				},
+			},
+		})
+        require("telescope").load_extension("ui-select")
 
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>pf", builtin.find_files, { desc = "Search File Names" })
 		vim.keymap.set("n", "<leader>po", builtin.oldfiles, { desc = "Search Old Files" })
 		vim.keymap.set("n", "<leader>pg", builtin.git_files, { desc = "Search Git Files" })
+		-- vim.keymap.set("n", "<leader>ps", builtin.live_grep, { desc = "Grep Search Project" })
 		vim.keymap.set("n", "<leader>ps", function()
 			builtin.grep_string({ search = vim.fn.input("Grep > ") })
 		end, { desc = "Grep Search Project" })
@@ -24,6 +33,41 @@ return {
 		vim.keymap.set("n", "<leader>sj", builtin.jumplist, { desc = "Search Jump-list" })
 		vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "Search Keymap" })
 		vim.keymap.set("n", "<leader>sh", builtin.man_pages, { desc = "Search Man Pages" })
+		vim.keymap.set("n", "<leader>sf", function()
+			local search_dir = vim.fn.input("Search directory: ", vim.fn.getcwd(), "dir")
+			if search_dir == "" then
+				search_dir = vim.fn.getcwd()
+			end
+
+			require("telescope.builtin").find_files({
+				cwd = search_dir,
+				attach_mappings = function(prompt_bufnr, map)
+					local actions = require("telescope.actions")
+					local action_state = require("telescope.actions.state")
+
+					actions.select_default:replace(function()
+						actions.close(prompt_bufnr)
+						local buf_dir = vim.fn.expand("%:p:h")
+						local selection = action_state.get_selected_entry()
+						local absolute = selection.path
+						local relative_buf = buf_dir ~= "" and ("." .. absolute:sub(#buf_dir + 1)) or absolute
+						local relative_cwd = vim.fn.fnamemodify(selection.path, ":p:.")
+						local filename = vim.fn.fnamemodify(selection.path, ":t")
+						local relative_home = vim.fn.fnamemodify(selection.path, ":p:~")
+
+						vim.ui.select({ relative_cwd, relative_buf, absolute, filename, relative_home }, {
+							prompt = "Copy to register:",
+						}, function(choice)
+							if choice then
+								vim.fn.setreg('"', choice)
+								vim.notify("Copied: " .. choice)
+							end
+						end)
+					end)
+					return true
+				end,
+			})
+		end, { desc = "Find & put Filename" })
 
 		vim.keymap.set("n", "<leader>gr", function()
 			builtin.lsp_references()
