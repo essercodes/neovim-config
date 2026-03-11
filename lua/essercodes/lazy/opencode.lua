127d3374f0dd7b3747dd64d80ca8c921d55cc9ce
Max Esser <max@esser.codes>
2026-03-11 16:30:44 -0500

    added keymap to select opencode agent

--- lua/essercodes/lazy/opencode.lua
+++ lua/essercodes/lazy/opencode.lua
@@ -1,21 +1,27 @@
 return {
-    "sudo-tee/opencode.nvim",
-    name = "opencode",
-    config = function()
-        require("opencode").setup({})
-    end,
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        {
-            "MeanderingProgrammer/render-markdown.nvim",
-            opts = {
-                anti_conceal = { enabled = false },
-                file_types = { 'markdown', 'opencode_output' },
-            },
-            ft = { 'markdown', 'Avante', 'copilot-chat', 'opencode_output' },
-        },
-        'saghen/blink.cmp',
-        'folke/snacks.nvim',
-        'nvim-telescope/telescope.nvim',
-    },
+	"sudo-tee/opencode.nvim",
+	name = "opencode",
+	config = function()
+		require("opencode").setup({
+			keymap = {
+				input_window = {
+					["<leader>oa"] = { "select_agent", mode = { "n" } },
+				},
+			},
+		})
+	end,
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{
+			"MeanderingProgrammer/render-markdown.nvim",
+			opts = {
+				anti_conceal = { enabled = false },
+				file_types = { "markdown", "opencode_output" },
+			},
+			ft = { "markdown", "Avante", "copilot-chat", "opencode_output" },
+		},
+		"saghen/blink.cmp",
+		"folke/snacks.nvim",
+		"nvim-telescope/telescope.nvim",
+	},
 }
