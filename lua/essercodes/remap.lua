4001c04712f1e2fba4512fbb293df93d604041dc
Max Esser <max@esser.codes>
2026-03-13 15:40:22 -0500

    fixed remap issue

--- lua/essercodes/remap.lua
+++ lua/essercodes/remap.lua
@@ -1,11 +1,8 @@
--- 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", {desc = "Move highlighted text down"})
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", {desc = "Move highlighted text up"})
 
--- 
 vim.keymap.set("n", "J", "mzJ`z", {desc = "Keep cursor in place when appending next line"})
 
--- Center cursor after moving
 vim.keymap.set("n", "<C-d>", "<C-d>zz", {desc = "Center cursor after half-page down"})
 vim.keymap.set("n", "<C-u>", "<C-u>zz", {desc = "Center cursor after half-page up"})
 vim.keymap.set("n", "n", "nzzze", {desc = "Center cursor after repeat last search"})
@@ -26,5 +23,5 @@ vim.keymap.set("t", "<C-w>j", [[<C-\><C-n><C-w>j]], {desc = "Break focus and mov
 vim.keymap.set("t", "<C-w>k", [[<C-\><C-n><C-w>k]], {desc = "Break focus and move to above pane"})
 vim.keymap.set("t", "<C-w>l", [[<C-\><C-n><C-w>l]], {desc = "Break focus and move to right pane"})
 
-vim.keymap.set("n", "<leader><leader>", "<C-w>", {"<C-w> window action alias"})
+vim.keymap.set("n", "<leader><leader>", "<C-w>", {desc = "<C-w> window action alias"})
 vim.keymap.set("n", "<leader>T", "<Cmd>below 20sp | terminal<CR>", {desc = "Open terminal split"})
