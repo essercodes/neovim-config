cedde405184dca5f73491b41423c34df66eb45c0
Max Esser <max@esser.codes>
2026-03-13 22:47:44 -0500

    backticks close in opencode

--- lua/essercodes/lazy/autopairs.lua
+++ lua/essercodes/lazy/autopairs.lua
@@ -1,15 +1,28 @@
 return {
-    {
-        'windwp/nvim-autopairs',
-        event = "InsertEnter",
-        config = true,
-        -- use opts = {} for passing setup options
-        -- this is equivalent to setup({}) function
-    },
-    {
-        -- Note that nvim-ts-autotag will not work unless you have treesitter 
-        -- parsers (like html) installed for a given filetype.
-        'windwp/nvim-ts-autotag',
-    },
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = function()
+			require("nvim-autopairs").setup({})
+			local npairs = require("nvim-autopairs")
+			local Rule = require("nvim-autopairs.rule")
+			local cond = require("nvim-autopairs.conds")
+            
+			npairs.add_rules({
+				Rule("```", "```", "opencode")
+                    :with_pair(cond.not_before_char("`", 3)),
+				Rule("```.*$", "```", "opencode")
+                    :only_cr()
+                    :use_regex(true),
+			})
+		end,
+		-- use opts = {} for passing setup options
+		-- this is equivalent to setup({}) function
+	},
+	{
+		-- Note that nvim-ts-autotag will not work unless you have treesitter
+		-- parsers (like html) installed for a given filetype.
+		"windwp/nvim-ts-autotag",
+	},
 }
 
