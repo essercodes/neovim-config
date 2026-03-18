37ba62b5fa00ac4db5766ec29d697a47a1998e21
Max Esser <max@esser.codes>
2026-03-18 15:26:53 -0500

    Added rust lsp server

--- lua/essercodes/lazy/lsp.lua
+++ lua/essercodes/lazy/lsp.lua
@@ -20,7 +20,7 @@ return {
 		require("mason-lspconfig").setup({
 			ensure_installed = {
 				"lua_ls",
-				-- "rust_analyzer",
+				"rust_analyzer",
 				-- "gopls",
 				"vtsls",
 				"tailwindcss",
