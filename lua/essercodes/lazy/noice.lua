ef1376812f37ac1ad20ec0b58b43093f7b009d04
Max Esser <max@esser.codes>
2026-03-19 14:07:27 -0500

    auto formatting

--- lua/essercodes/lazy/noice.lua
+++ lua/essercodes/lazy/noice.lua
@@ -7,14 +7,14 @@ return {
 			hover = { enabled = false },
 			override = {
 				["vim.lsp.util.stylize_markdown"] = true,
-                ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
 			},
 		},
 		presets = {
 			bottom_search = true,
 			command_palette = true,
 			long_message_to_split = true,
-            inc_rename = false,
+			inc_rename = false,
 		},
 		routes = {},
 	},
