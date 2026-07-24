d9f6d9ffad71f074f42b73a18bace7d974dff26c
Max Esser <max@esser.codes signingkey = ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCdWqfMb1oJQVsMl4Ve73vZpoFpmpQnAYX07z/yKHAJKE0Lnc1IvGDGDysw5PW0UpMbNNxQlTCjnEl85JSzjKBQ6cBvQ9XybNEniRUPGniV/j8lLe1AjwN/9hc84zI+cJwt7yHpayUNgrLXz+heWVTXHuVSy7jXpLvFg1NH0hTMJRANL3KUuvIrI9GvYxDmxeuxqFpceDeMyYJ9YSAVIzLtddjtzN/Y0aBvOgD6Jw6QLZ2c96Ly11j6LNuPhZyqWAf8i5KurCCf0AQnDKl9vGHDhkuwlMBMa/FSB2N2fb18KXCHpHtRypJDNuxeTVUVrmOhWAOCiCZKodaPvtF7SC8CeqpvLOqHpKRLCVZtWZYdg9gesCOP/5BUCASZhlrqeA1pmc0SFB753HgxJrHWG1f/UR+KfMP4ls70ZrHpevUpeGvVdS4yZQ7CDEZWoVnn74QyLJKGF959KWRn1UPbWKf+1P5+5fxcxKX6gmMrNeXH7DK/mWcOvhjz8K2Aumlk9bM=>
2026-07-24 00:30:18 -0500

    activate venv early

--- devcontainer-feature/neovim-config/install.sh
+++ devcontainer-feature/neovim-config/install.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 set -eu
-# PYTHON_VENV="/home/ubuntu/jupytext-venv"
+PYTHON_VENV="/home/ubuntu/jupytext-venv"
+
 echo "Activating feature 'Neovim Config'"
 
-# Install Treesitter Dependences
+echo "Create and Activate VENV"
+python3 -m venv $PYTHON_VENV
+source $PYTHON_VENV/bin/activate
+
+echo "Install Treesitter Dependencies"
 apt-get -y update
 apt-get install -y clang
 cargo install --locked tree-sitter-cli
 
-# Install Jupytext Plugin Dependences
-# python3 -m venv $PYTHON_VENV
-# source $PYTHON_VENV/bin/activate
+echo "Install Jupytext Plugin Dependencies"
+pip install jupytext
 
-# pip install jupytext
-
-# Install Agents
+echo "Install Agents"
 curl -fsSL https://claude.ai/install.sh | bash
 curl -fsSL https://opencode.ai/install | bash
 
+echo "Clone Config"
 git clone https://github.com/essercodes/neovim-config /home/ubuntu/.config/nvim
+
+echo "Install Neovim Plugins and Tools"
 sudo -u ubuntu nvim --headless "+Lazy! sync" +qa
 sudo -u ubuntu nvim --headless -c 'MasonToolsInstallSync' -c 'qa'
