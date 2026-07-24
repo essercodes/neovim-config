9512e53c31ad61cd09e7927d5c3839fa2eeb0ae0
Max Esser <max@esser.codes signingkey = ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCdWqfMb1oJQVsMl4Ve73vZpoFpmpQnAYX07z/yKHAJKE0Lnc1IvGDGDysw5PW0UpMbNNxQlTCjnEl85JSzjKBQ6cBvQ9XybNEniRUPGniV/j8lLe1AjwN/9hc84zI+cJwt7yHpayUNgrLXz+heWVTXHuVSy7jXpLvFg1NH0hTMJRANL3KUuvIrI9GvYxDmxeuxqFpceDeMyYJ9YSAVIzLtddjtzN/Y0aBvOgD6Jw6QLZ2c96Ly11j6LNuPhZyqWAf8i5KurCCf0AQnDKl9vGHDhkuwlMBMa/FSB2N2fb18KXCHpHtRypJDNuxeTVUVrmOhWAOCiCZKodaPvtF7SC8CeqpvLOqHpKRLCVZtWZYdg9gesCOP/5BUCASZhlrqeA1pmc0SFB753HgxJrHWG1f/UR+KfMP4ls70ZrHpevUpeGvVdS4yZQ7CDEZWoVnn74QyLJKGF959KWRn1UPbWKf+1P5+5fxcxKX6gmMrNeXH7DK/mWcOvhjz8K2Aumlk9bM=>
2026-07-24 00:17:24 -0500

    version 0.1.2

--- devcontainer-feature/neovim-config/install.sh
+++ devcontainer-feature/neovim-config/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -eu
-PYTHON_VENV="/home/ubuntu/jupytext-venv"
+# PYTHON_VENV="/home/ubuntu/jupytext-venv"
 echo "Activating feature 'Neovim Config'"
 
 # Install Treesitter Dependences
@@ -9,8 +9,8 @@ apt-get install -y clang
 cargo install --locked tree-sitter-cli
 
 # Install Jupytext Plugin Dependences
-python3 -m venv $PYTHON_VENV
-source $PYTHON_VENV/bin/activate
+# python3 -m venv $PYTHON_VENV
+# source $PYTHON_VENV/bin/activate
 
 # pip install jupytext
 
